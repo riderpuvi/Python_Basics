9ea0dc78b2971c5728d43abb37edecb9d5c6a520
riderpuvi <v_puneeth@lilly.com>
2022-09-14 12:24:42 +0530

    Minor Changes

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 
 name=input("Enter your name : ")
-while len(name)==0 or name.isalpha()== False:
+while len(name)==0 or name.isalpha()==False:
      name=input("Enter Name which has only alphabets and Not empty : ")
 
 phone=input("Enter phone Number : ")
@@ -8,7 +8,7 @@ while len(phone)!=10 or phone.isnumeric()==False:
     phone=input("Enter Valid Telephone Number : ")
 
 age=input("Enter Your Age : ")
-while int(age)<20 or int(age)>100  or age.isnumeric()==False:
+while len(age)==0 or age.isnumeric()==False or int(age)<20 or int(age)>100 :
      age=(input("Enter Valid Age : "))
 
 sal=input("Enter your salary : ")
@@ -16,7 +16,7 @@ while  len(sal)==0 or  sal.isnumeric()==False or int(sal)<0:
     sal=input("Enter Valid salary: ") 
 
 city=input("Enter your city: ")
-while city.isalpha()==False or len(city)==0:
+while len(city)==0 or  city.isalpha()==False :
     city=input("Enter valid City name: ")
 
 dept=input("Enter Your Department: ")
@@ -29,4 +29,3 @@ print("Age:", age)
 print("Sal:", sal)
 print("City:", city)
 print("Dept:", dept)
-
