51c580317daa9907c8a588d4de26e97e37f5cbb2
riderpuvi <v_puneeth@lilly.com>
2022-09-15 10:15:39 +0530

    After  DB Connection

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
+import sys
+import mariadb
+conn = mariadb.connect(
 
+    user="root",
+
+    password="puneeth@123",
+
+    host="localhost",
+
+    port = 3306,
+
+    database = "python",
+
+)
+
+cur= conn.cursor()
+conn.autocommit=True
 name=input("Enter your name : ")
 while len(name)==0 or name.isalpha()==False:
      name=input("Enter Name which has only alphabets and Not empty : ")
@@ -23,9 +40,29 @@ dept=input("Enter Your Department: ")
 while len(dept)==0 or dept.isalpha()==False:
     dept=input("Enter Valid Department: ") 
 
-print("\nName:", name)
-print("Phone:", phone)
-print("Age:", age)
-print("Sal:", sal)
-print("City:", city)
-print("Dept:", dept)
+try:
+
+    cur.execute("create table emp (name varchar(255), phone varchar(10), age int, sal int, city varchar(255), dept varchar(255));")
+
+except Exception as y:
+
+    print(y)
+
+query = """insert into emp(name,phone,age,sal,city,dept) values(%s,%s,%d,%d,%s,%s)"""
+
+record = (name,phone,int(age),int(sal),city,dept)
+
+cur.execute(query,record)
+
+cur.execute("select name, phone, age, sal,city,dept from emp;")
+
+for (name,phone,age,sal,city,dept) in cur:
+    print("Name:", {name},"Phone:", {phone},"Age:", {age},"Salary:", {sal},"City:", {city},"Dept:", {dept})
+
+# print("\nName:", name)
+# print("Phone:", phone)
+# print("Age:", age)
+# print("Sal:", sal)
+# print("City:", city)
+# print("Dept:", dept)
+
